5c8a5951ed2e10f1e8d4374ec8c35032bb9186c8
Al Hasanul Banna <tutul5800@gmail.com>
2023-05-15 21:06:52 +0600

    slider fixed

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -6,9 +6,26 @@ import banner4 from "../../../assets/images/banner/4.jpg";
 
 const Banner = () => {
   return (
-    <div className="carousel w-full">
+    <div className="carousel w-full h-[600px]">
       <div id="slide1" className="carousel-item relative w-full">
-        <img src={banner1} className="w-full" />
+        <img src={banner1} className="w-full rounded-xl" />
+        <div className="absolute flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[gradient(rgba(21, 21, 21, 0)]">
+          <div className="text-white space-y-7 w-2/3 pl-10">
+            <h2 className="text-6xl font-bold">
+              Affordable Price For Car Servicing
+            </h2>
+            <p>
+              There are many variations of passages of available, but the
+              majority have suffered alteration in some form
+            </p>
+            <div>
+              <button className="btn btn-secondary mr-4">Discover More</button>
+              <button class="btn btn-outline btn-primary">
+                Latest Projects
+              </button>
+            </div>
+          </div>
+        </div>
         <div className="absolute flex transform justify-end -translate-y-1/2 left-5 right-5 bottom-0">
           <a href="#slide4" className="btn btn-circle mr-4">
             ❮
